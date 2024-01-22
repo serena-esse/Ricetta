a52b57c19f3ec6526c55662856e1a534b5da93d0
Serena Siliberti <serena.siliberti@gmail.com>
2024-01-22 18:55:29 +0100

    procedimento

--- ricetta.js
+++ ricetta.js
@@ -1,3 +1,6 @@
+let titolo = "Ricetta Tiramisù";
+console.log(titolo);
+
 let ing1 = "mascarpone";
 let ing2 = "savoiardi";
 let ing3 = "caffè";
@@ -6,4 +9,26 @@ let ing5 = "zucchero";
 let ing6 = "cacaoAmaro";
 
 let ingredienti = ing1 + " " + ing2 + " " + ing3 + " " + ing4 + " " + ing5 + " " + ing6;
-console.log(ingredienti);
+console.log("ingredienti:", ingredienti);
+
+let n1 = "Preparare il caffè.";
+let n2 = "Separare gli albumi dai tuorli.";
+let n3 = "Montare i tuorli con le fruste elettriche,versando solo metà dose di zucchero.";
+let n4 =
+  "Non appena il composto sarà diventato chiaro e spumoso, e con le fruste ancora in funzione, potrete aggiungere il mascarpone, poco alla volta.";
+let n5 = "Incorporato tutto il formaggio avrete ottenuto una crema densa e compatta; tenetela da parte. ";
+let n6 =
+  "Pulite molto bene le fruste e passate a montare gli albumi. Quando saranno schiumosi versate il restante zucchero un po’ alla volta.";
+let n7 =
+  "Prendete una cucchiaiata di albumi e versatela nella ciotola con la crema di mascarpone e mescolate energicamente con una spatola, così stempererete il composto. Dopodiché procedete ad aggiungere la restante parte di albumi, poco alla volta mescolando molto delicatamente dal basso verso l'alto";
+let n8 =
+  "La crema al mascarpone è ora pronta. Distribuitene una generosa cucchiaiata sul fondo di una pirofila di vetro, grande 30x19,5cm e distribuite per bene su tutta la base. Inzuppate per pochi istanti i savoiardi nel caffè freddo prima da un lato e poi dall’altro";
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+console.log(n8);
