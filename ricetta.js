f46219f7120fedd2ee5eb54420967b6012bba9fd
Serena Siliberti <serena.siliberti@gmail.com>
2024-01-22 18:31:26 +0100

    ingredienti

---
+++ ricetta.js
@@ -0,0 +1,9 @@
+let ing1 = "mascarpone";
+let ing2 = "savoiardi";
+let ing3 = "caffè";
+let ing4 = "uova";
+let ing5 = "zucchero";
+let ing6 = "cacaoAmaro";
+
+let ingredienti = ing1 + " " + ing2 + " " + ing3 + " " + ing4 + " " + ing5 + " " + ing6;
+console.log(ingredienti);
